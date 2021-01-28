04995b330f8eb1d562ad9e6ce1b629900a2ca61d
zhuxuan <zhuxuan@igengmei.com>
2021-01-28 15:16:05 +0800

    fix

--- test/CityFilterApi.dart
+++ test/CityFilterApi.dart
@@ -5,11 +5,11 @@
  * @Last Modified time: 2021-01-06 11:08:12
  */
 import 'package:flutter_common/Annotations/anno/Get.dart';
-import 'package:flutter_common/Annotations/anno/Post.dart';
 import 'package:flutter_common/Annotations/anno/Query.dart';
 import 'package:flutter_common/Annotations/anno/ServiceCenter.dart';
-import 'package:gm_flutter/CityFilterModel/service/entity/CityFilterBean.dart';
-import 'package:gm_flutter/CityFilterModel/service/entity/CityFilterResultBean.dart';
+import 'CityFilterBean.dart';
+import 'CityFilterResultBean.dart';
+// import 'package:fl';
 
 @ServiceCenter()
 abstract class CityFilterApi {
@@ -17,5 +17,6 @@ abstract class CityFilterApi {
   CityFilterBean getAllCities();
 
   @Get("api/city/home_search")
-  CityFilterResultBean getFilterCities(@Query("keyword") String keyword, int num, bool isShow);
+  CityFilterResultBean getFilterCities(
+      @Query("keyword") String keyword, int num, bool isShow);
 }
--- test/CityFilterResultBean.dart
+++ test/CityFilterResultBean.dart
@@ -4,8 +4,6 @@
  * @Last Modified by: zx
  * @Last Modified time: 2021-01-05 15:02:48
  */
-import 'package:gm_flutter/commonModel/bean/Area.dart';
-
 class CityFilterResultBean {
   String message;
   List<Cities> data;
@@ -31,6 +29,44 @@ class CityFilterResultBean {
       data['data'] = this.data.map((v) => v.toJson()).toList();
     }
     data['error'] = this.error;
+    return data;
+  }
+}
+
+class Cities {
+  String cityName;
+  String id;
+  String name;
+  int cityTagId;
+  String highlightedName;
+  bool isLocatedCity;
+
+  Cities(
+      {this.cityName,
+      this.id,
+      this.name,
+      this.cityTagId,
+      this.highlightedName,
+      this.isLocatedCity});
+
+  Cities.fromJson(Map<String, dynamic> json) {
+    highlightedName = json['highlighted_name'];
+    cityName = json['city_name'];
+    id = json['id'] == null ? "" : "${json['id']}";
+    name = json['name'];
+    cityTagId = json['city_tag_id'];
+    isLocatedCity =
+        json['is_located_city'] == null ? false : json['is_located_city'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['city_name'] = this.cityName;
+    data['highlighted_name'] = this.highlightedName;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['city_tag_id'] = this.cityTagId;
+    data['is_located_city'] = this.isLocatedCity;
     return data;
   }
 }
